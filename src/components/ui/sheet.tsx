00588b09197ef8195263cb29bddcc432b8888501
Tevin Owino <tevohbazuh@gmail.com>
2025-05-10 21:42:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: `DialogContent` requires a `DialogTitle` for the component to be accessible for screen reader users.
    >
    > If you want to hide the `DialogTitle`, you can wrap it with our VisuallyHidden component.
    >
    > For more information, see https://radix-ui.com/primitives/docs/components/dialog. Error source: src/components/ui/sheet.tsx (62:5) @ _c1
    >
    >   60 |   <SheetPortal>
    >   61 |     <SheetOverlay />
    > > 62 |     <SheetPrimitive.Content
    >      |     ^
    >   63 |       ref={ref}
    >   64 |       className={cn(sheetVariants({ side }), className)}
    >   65 |       {...props}
    >
    > Call Stack
    > 27
    >
    > Show 23 ignore-listed frame(s)
    > _c1
    > src/components/ui/sheet.tsx (62:5)
    > _c1
    > src/components/ui/sheet.tsx (60:3)
    > Navbar
    > src/components/shared/Navbar.tsx (141:13)
    > RootLayout
    > src/app/layout.tsx (49:15)

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -4,6 +4,8 @@ import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden"
+
 
 import { cn } from "@/lib/utils"
 
@@ -64,6 +66,9 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
+      <VisuallyHidden asChild>
+        <SheetPrimitive.Title>Mobile Menu</SheetPrimitive.Title>
+      </VisuallyHidden>
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
